3d08d07ce27b2ebe44905eccc5470a8ff87998ba
Isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-07-28 16:22:19 -0300

    feat: diretiva NgClass com base condicional

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    RouterOutlet
+  ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
+
 export class BodyComponent {
+  @Input() collapsed: boolean = false;
+  @Input() screenWidth: number = 0;
+
+  public getBodyClass(): string {
+    let styleClass = '';
+    if (this.collapsed && this.screenWidth > 768) {
+      styleClass = 'body-trimmed';
+    } else if (this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0) {
+      styleClass = 'body-md-screen';
+    }
+    return styleClass
+  }
 
 }
