df7f0d4d57dd505768d67a5cee356ad61a38a52b
Isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-07-28 17:29:16 -0300

    feat: animando sidenav

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -3,6 +3,7 @@ import { navbarData } from './navbar/sidenav-data';
 import { NgClass, NgFor, NgIf } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SideNavToggle } from '../../interfaces/sidenav';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-sidenav',
@@ -15,7 +16,28 @@ import { SideNavToggle } from '../../interfaces/sidenav';
     NgClass
   ],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+  styleUrl: './sidenav.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('350ms', style({opacity: 1}))
+      ]),
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('350ms', style({opacity: 0}))
+      ])
+    ]),
+
+    trigger('rotate', [
+      transition(':enter', [
+        animate('1000ms', keyframes([
+          style({transform: 'rotate(0deg)', offset: 0}),
+          style({transform: 'rotate(2turn)', offset: 1})
+        ]))
+      ])
+    ])
+  ]
 })
 
 export class SidenavComponent  implements OnInit{
