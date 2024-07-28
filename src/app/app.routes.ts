bac7eed8a3c55c5ac4b575a859dba9e540279b03
Isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-07-28 17:08:26 -0300

    feat: alterando o comportamento do sidenav conforme a largura da janela

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { ProductsComponent } from './views/products/products.component';
+import { StatisctsComponent } from './views/statiscts/statiscts.component';
+import { CoupensComponent } from './views/coupens/coupens.component';
+import { PageComponent } from './views/page/page.component';
+import { MediaComponent } from './views/media/media.component';
+import { SettingsComponent } from './views/settings/settings.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +17,35 @@ export const routes: Routes = [
         path: 'dashboard',
         component: DashboardComponent,
         title: 'Dashboard'
-    }
+    },
+    {
+        path: 'products',
+        component: ProductsComponent,
+        title: 'Products'
+    },
+    {
+        path: 'statistics',
+        component: StatisctsComponent,
+        title: 'Statistics'
+    },
+    {
+        path: 'coupens',
+        component: CoupensComponent,
+        title: 'Coupens'
+    },
+    {
+        path: 'pages',
+        component: PageComponent,
+        title: 'Pages'
+    },
+    {
+        path: 'media',
+        component: MediaComponent,
+        title: 'Media'
+    },
+    {
+        path: 'settings',
+        component: SettingsComponent,
+        title: 'Settings'
+    },
 ];
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 import { navbarData } from './navbar/sidenav-data';
 import { NgClass, NgFor, NgIf } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
@@ -18,13 +18,27 @@ import { SideNavToggle } from '../../interfaces/sidenav';
   styleUrl: './sidenav.component.scss'
 })
 
-export class SidenavComponent {
+export class SidenavComponent  implements OnInit{
   @Output() onToggleSidenav: EventEmitter<SideNavToggle> = new EventEmitter();
 
   protected collapsed: boolean = false;
   protected screenwidth: number = 0;
 
   protected navData = navbarData;
+
+  /* Ajustando o sidenav automaticamente conforme a largura da janela. */
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): any {
+    this.screenwidth = window.innerWidth;
+    if (this.screenwidth <= 768) {
+      this.collapsed = false;
+      this.onToggleSidenav.emit({collapsed: this.collapsed, screenWidth: this.screenwidth});
+    }
+  }
+
+  ngOnInit(): void {
+    this.screenwidth = window.innerWidth; // Fazendo o tributo `screenWidth receber a largura da janela`
+  }
 
   public toggleCollapsed(): void {
     this.collapsed = !this.collapsed;
