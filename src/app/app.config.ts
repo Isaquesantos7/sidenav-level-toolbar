8826dac4442c1bef82813dd072b812f5fb9e3434
Isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-07-30 20:22:01 -0300

    feat: toolbar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FullscreenOverlayContainer, OverlayContainer } from '@angular/cdk/overlay';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), {provide: OverlayContainer, useClass: FullscreenOverlayContainer} ]
 };
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,19 +1,38 @@
-import { NgClass } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CdkMenuModule } from '@angular/cdk/menu';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    NgIf,
+    OverlayModule,
+    CdkMenuModule
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   @Input() collapsed:boolean = false;
   @Input() screenWidth: number = 0;
+
+  protected canShowSearchAsOverlay: boolean = false;
+
+  constructor () {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkCanShowSearchAsOverlay(window.innerWidth);
+  }
+
+  ngOnInit(): void {
+    this.checkCanShowSearchAsOverlay(window.innerWidth);
+  }
 
   public getHeadClass(): string {
     let styleClass = '';
@@ -24,5 +43,13 @@ export class ToolbarComponent {
       styleClass = 'head-md-screen';
     }
     return styleClass;
+  }
+
+  public checkCanShowSearchAsOverlay(innerWitdh: number): void {
+    if (innerWitdh < 845) {
+      this.canShowSearchAsOverlay = true;
+    } 
+
+    this.canShowSearchAsOverlay = false;
   }
 }
